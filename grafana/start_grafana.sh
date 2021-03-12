8c8315857b3f7e445dcdc46aa22eb146278e9137
percylechat <44782706+percylechat@users.noreply.github.com>
2021-03-12 21:13:47 +0100

    Add files via upload

--- grafana/start_grafana.sh
+++ grafana/start_grafana.sh
@@ -2,6 +2,6 @@
 rm etc/grafana.ini
 rm etc/conf.d/grafana
 cp grafana etc/conf.d/
-cp grafana.ini etc/
+cp grafana.ini usr/share/grafana/conf
 nohup /usr/sbin/grafana-server --config=/usr/share/grafana/conf/grafana.ini --homepath=/usr/share/grafana 0</dev/null
 sleep infinity & wait
